555eef5685639630046c328a8ac1ce4d33d1045e
ftnext <takuyafjp+develop@gmail.com>
2022-04-20 00:43:31 +0900

    [test] Xth day border case

--- x-days-anniversary/tests/test_core.py
+++ x-days-anniversary/tests/test_core.py
@@ -12,3 +12,11 @@ class XthDayCountTestCase(TestCase):
 
         # 2021/10/29の次の週が2021/11/5なので、2021/11/4は7日目
         self.assertEqual(actual, 7)
+
+    def test_calculate_with_anniversary_itself(self):
+        sut = XthDayCount(date(2022, 4, 19))
+
+        actual = sut(date(2022, 4, 19))
+
+        # 2022/4/19自身は1日目（例えば公開日は初日なので1日目）
+        self.assertEqual(actual, 1)
