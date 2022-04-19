9ff61a137e4bf60aa456a0ff001fa8eddc1ee18d
ftnext <takuyafjp+develop@gmail.com>
2022-04-20 00:55:30 +0900

    [feat] Calculate remaining days (not including goal)

--- x-days-anniversary/src/core.py
+++ x-days-anniversary/src/core.py
@@ -8,3 +8,13 @@ class XthDayCount:
     def __call__(self, point: date) -> int:
         delta = point - self._anniversary
         return delta.days + 1
+
+
+class DayCountDown:
+    def __init__(self, goal: date, *, include: bool):
+        self._goal = goal
+        self._include = include
+
+    def __call__(self, point: date) -> int:
+        delta = self._goal - point
+        return delta.days
