b43df9085b48f706ae5cb09bf9d0469ff0022fd9
ftnext <takuyafjp+develop@gmail.com>
2024-08-03 19:44:11 +0900

    [feat] Can use unaware of date order

--- x-days-anniversary/rust_practice/day-counter/src/main.rs
+++ x-days-anniversary/rust_practice/day-counter/src/main.rs
@@ -5,13 +5,23 @@ fn main() {
 }
 
 fn count_days(date1: &NaiveDate, date2: &NaiveDate) -> i64 {
+    if date1 < date2 {
+        return count_days(date2, date1);
+    }
     let duration = date1.signed_duration_since(*date2);
     duration.num_days()
 }
 
 #[test]
-fn test_count_days() {
+fn test_count_days_passed_bigger_smaller_order() {
     let date1 = NaiveDate::from_ymd_opt(2023, 6, 28).unwrap();
     let date2 = NaiveDate::from_ymd_opt(2023, 6, 17).unwrap();
+    assert_eq!(count_days(&date1, &date2), 11);
+}
+
+#[test]
+fn test_count_days_passed_smaller_bigger_order() {
+    let date1 = NaiveDate::from_ymd_opt(2023, 6, 17).unwrap();
+    let date2 = NaiveDate::from_ymd_opt(2023, 6, 28).unwrap();
     assert_eq!(count_days(&date1, &date2), 11);
 }
