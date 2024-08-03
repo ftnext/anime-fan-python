6006fa943eaeb966f8dffdb86f7aa7b3ed7ecdc9
ftnext <takuyafjp+develop@gmail.com>
2024-08-03 19:40:41 +0900

    [feat] Count days function
    ref: https://nikkie-ftnext.hatenablog.com/entry/reimplement-day-counter-as-rust-practice

--- x-days-anniversary/rust_practice/day-counter/src/main.rs
+++ x-days-anniversary/rust_practice/day-counter/src/main.rs
@@ -1,3 +1,17 @@
+use chrono::NaiveDate;
+
 fn main() {
     println!("Hello, world!");
+}
+
+fn count_days(date1: &NaiveDate, date2: &NaiveDate) -> i64 {
+    let duration = date1.signed_duration_since(*date2);
+    duration.num_days()
+}
+
+#[test]
+fn test_count_days() {
+    let date1 = NaiveDate::from_ymd_opt(2023, 6, 28).unwrap();
+    let date2 = NaiveDate::from_ymd_opt(2023, 6, 17).unwrap();
+    assert_eq!(count_days(&date1, &date2), 11);
 }
