2d58b30a367a6a76c7f3fb3b4a2fce7b0f26f8d2
ftnext <takuyafjp+develop@gmail.com>
2024-08-03 20:00:13 +0900

    [feat] Count down concert

--- x-days-anniversary/rust_practice/day-counter/src/main.rs
+++ x-days-anniversary/rust_practice/day-counter/src/main.rs
@@ -1,7 +1,14 @@
-use chrono::NaiveDate;
+use chrono::{Datelike, Local, NaiveDate};
 
 fn main() {
-    println!("Hello, world!");
+    let now: NaiveDate = Local::now().date_naive();
+    let kojo_concert = NaiveDate::from_ymd_opt(now.year(), 8, 8).unwrap();
+    println!(
+        "『かがみの孤城』のコンサートの {} まで、あと{}日です",
+        kojo_concert.format("%m/%d"),
+        count_days(&kojo_concert, &now)
+    );
+    println!("Concert {} - Today {}", kojo_concert, now);
 }
 
 fn count_days(date1: &NaiveDate, date2: &NaiveDate) -> i64 {
